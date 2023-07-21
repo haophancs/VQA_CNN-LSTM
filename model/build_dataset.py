108bd8df4cd09df7075623f42c4e33dde2b721a5
haophancs <haophan.cs.uit@gmail.com>
2023-07-22 01:45:42 +0700

    edit to preprocess viclevr

--- model/build_dataset.py
+++ model/build_dataset.py
@@ -30,6 +30,7 @@ class VQADataset(Dataset):
         qu_id = self.input_data[idx]['qu_id']
         qu_tokens = self.input_data[idx]['qu_tokens']
         qu2idx = np.array([self.qu_vocab.word2idx('<pad>')] * self.max_qu_len)
+        print(len(qu2idx), len(qu_tokens))
         qu2idx[:len(qu_tokens)] = [self.qu_vocab.word2idx(token) for token in qu_tokens]
         sample = {'image': img, 'question': qu2idx, 'question_id': qu_id}
 
